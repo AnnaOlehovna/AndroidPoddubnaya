f7589a2945d0ad3357d20b04b5ef9222d396a8e6
AnnaOlehovna <anya.olehovna@gmail.com>
2018-04-02 11:56:28 +0300

    fixed inageview

--- Test/presentation/src/main/java/com/olehovnagmail/anya/presentation/screens/homework6/WeatherItemViewModel.java
+++ Test/presentation/src/main/java/com/olehovnagmail/anya/presentation/screens/homework6/WeatherItemViewModel.java
@@ -3,20 +3,18 @@ package com.olehovnagmail.anya.presentation.screens.homework6;
 
 import android.databinding.BindingAdapter;
 import android.databinding.ObservableField;
+import android.databinding.ObservableInt;
 import android.widget.ImageView;
 
 import com.olehovnagmail.anya.R;
 import com.olehovnagmail.anya.domain.homework6.weatherEntity.CityEntity;
 import com.olehovnagmail.anya.domain.homework6.weatherEntity.WeatherEntity;
 
-import io.reactivex.functions.Consumer;
-import io.reactivex.subjects.PublishSubject;
 
 public class WeatherItemViewModel {
 
-    PublishSubject<WeatherEntity> subject = PublishSubject.create();
-
-    public ObservableField<String> title = new ObservableField<>("");
+    public ObservableField<String> title = new ObservableField<>();
+    public ObservableInt imageRes = new ObservableInt();
     public ObservableField<String> description = new ObservableField<>();
     public ObservableField<String> date = new ObservableField<>();
     public ObservableField<String> location = new ObservableField<>();
@@ -25,43 +23,38 @@ public class WeatherItemViewModel {
     public ObservableField<String> humidity = new ObservableField<>();
 
     public void setWeatherEntity(WeatherEntity weatherEntity) {
-        subject.subscribe(new Consumer<WeatherEntity>() {
-            @Override
-            public void accept(WeatherEntity weatherEntity) throws Exception {
-                title.set(weatherEntity.getTitle());
-                description.set(weatherEntity.getDescription());
-                date.set(weatherEntity.getDate());
-                String locationString = "Country: ";
-                for (String country : weatherEntity.getLocation().keySet()) {
-                    locationString = locationString.concat(country);
-                    for (CityEntity city:weatherEntity.getLocation().get(country) ) {
-                        locationString = locationString.concat("\n\t"+city.getName());
-                    }
-                }
-                location.set(locationString);
-                minTemp.set("Min temperature: ".concat(String.valueOf(weatherEntity.getTempMin())));
-                maxTemp.set("Max temperature: ".concat(String.valueOf(weatherEntity.getTempMax())));
-                humidity.set("Humidity: ".concat(String.valueOf(weatherEntity.getHumidity())));
-            }
-        });
-        subject.onNext(weatherEntity);
-    }
-
-    @BindingAdapter("android:src")
-    public static void setImage(ImageView view, String title){
-        switch (title.toLowerCase()) {
+        switch (weatherEntity.getTitle().toLowerCase()) {
             case "sunny":
             case "clear":
-                view.setImageResource(R.drawable.sunny);
+                imageRes.set(R.drawable.sunny);
                 break;
             case "clouds":
-                view.setImageResource(R.drawable.cloud);
+                imageRes.set(R.drawable.cloud);
                 break;
             case "rain":
-                view.setImageResource(R.drawable.rain);
+                imageRes.set(R.drawable.rain);
                 break;
             default:
                 break;
         }
+        description.set(weatherEntity.getDescription());
+        date.set(weatherEntity.getDate());
+        String locationString = "Country: ";
+        for (String country : weatherEntity.getLocation().keySet()) {
+            locationString = locationString.concat(country);
+            for (CityEntity city : weatherEntity.getLocation().get(country)) {
+                locationString = locationString.concat("\n\t" + city.getName());
+            }
+        }
+        location.set(locationString);
+        minTemp.set("Min temperature: ".concat(String.valueOf(weatherEntity.getTempMin())));
+        maxTemp.set("Max temperature: ".concat(String.valueOf(weatherEntity.getTempMax())));
+        humidity.set("Humidity: ".concat(String.valueOf(weatherEntity.getHumidity())));
+    }
+
+
+    @BindingAdapter("android:src")
+    public static void setImage(ImageView view, int resId) {
+        view.setImageResource(resId);
     }
 }
